7e0ae91be42db2b61dc1a03f7741eadb0640751e
belg04 <113061405+belg04@users.noreply.github.com>
2024-03-03 15:56:47 +0200

    Добавьте файлы проекта.

---
+++ lab6 2/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab6_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(txtInputReal.Text, out double number))
+            {
+                lbQueqe.Items.Add(number);
+                QueqeResult.Text = $"Вещественное число {number} добавлено в список.";
+            }
+            else
+            {
+                MessageBox.Show("введите корректное вещественное число.");
+            }
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            int negativeCount = 0;
+            foreach (var item in lbQueqe.Items)
+            {
+                if (item is double db && db < 0)
+                {
+                    negativeCount++;
+                }
+            }
+            QueqeResult.Text = $"Количество отрицательных чисел в списке: {negativeCount}";
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+
+
+    
+        }
+    }
+
+
+}
